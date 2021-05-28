7e3634b643fcd6c4e976b52860c00d7d8f40672c
yulva <82402793+yulva@users.noreply.github.com>
2021-05-28 15:35:09 +0400

    Add files via upload

---
+++ example_lesson11.sql
@@ -0,0 +1,63 @@
+-- Практическое задание по теме “Оптимизация запросов”
+-- Задание 1
+CREATE TABLE Logs (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    created_at datetime DEFAULT CURRENT_TIMESTAMP,
+    table_name varchar(50) NOT NULL,
+    row_id INT UNSIGNED NOT NULL,
+    row_name varchar(255)
+) ENGINE = Archive;
+
+CREATE TRIGGER products_insert AFTER INSERT ON products
+FOR EACH ROW
+BEGIN
+    INSERT INTO Logs VALUES (NULL, DEFAULT, "products", NEW.id, NEW.name);
+END;
+
+CREATE TRIGGER users_insert AFTER INSERT ON users
+FOR EACH ROW
+BEGIN
+    INSERT INTO Logs VALUES (NULL, DEFAULT, "users", NEW.id, NEW.name);
+END;
+
+CREATE TRIGGER catalogs_insert AFTER INSERT ON catalogs
+FOR EACH ROW
+BEGIN
+    INSERT INTO Logs VALUES (NULL, DEFAULT, "catalogs", NEW.id, NEW.name);
+END;
+-- ----------------------------------------------------------------------------------------
+
+
+-- 2. Задание 2
+CREATE TABLE samples (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) COMMENT 'Имя покупателя',
+  birthday_at DATE COMMENT 'Дата рождения',
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP) 
+  COMMENT = 'Покупатели';
+
+INSERT INTO samples (name, birthday_at) VALUES
+  ('Геннадий', '1990-10-05'),
+  ('Наталья', '1984-11-12'),
+  ('Александр', '1985-05-20'),
+  ('Сергей', '1988-02-14'),
+  ('Иван', '1998-01-12'),
+  ('Мария', '1992-08-29'),
+  ('Аркадий', '1994-03-17'),
+  ('Ольга', '1981-07-10'),
+  ('Владимир', '1988-06-12'),
+  ('Екатерина', '1992-09-20');
+
+SELECT COUNT(*) FROM
+  samples AS fst,
+  samples AS snd,
+  samples AS thd,
+  samples AS fth,
+  samples AS fif,
+  samples AS sth;
+
+SELECT COUNT(*) FROM users;
+
+SELECT * FROM users LIMIT 10;
+-- ----------------------------------------------------------------------------------------
